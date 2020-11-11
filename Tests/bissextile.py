99342d97d1243705feb821b94c880a6a8dc9403f
Louis-Justin TALLOT <lj.tallot@gmail.com>
2020-11-11 17:45:11 +0100

    Ce programme détermine si une année donnée est bissextile ou non

---
+++ Tests/bissextile.py
@@ -0,0 +1,10 @@
+# ﻿ -*-coding:utf-8 -*
+
+import os
+annee = input("Saisissez une année : ")
+annee = int(annee)
+if annee % 400 == 0 or (annee % 4 == 0 and annee % 100 != 0):
+	print("L'année saisie est bissextile.")
+else:
+	print("L'année saisie n'est pas bissextile.")
+input()
