c77606c6d6fe60a862f406825fe9726f3f00c2e2
Louis-Justin TALLOT <lj.tallot@gmail.com>
2020-11-11 17:41:29 +0100

    Une tentative (recopiée) de Dijsktra qui ne marche pas du tout

---
+++ Algorithmes_classiques/Dijsktra_echec.py
@@ -0,0 +1,50 @@
+###### ECHEC ####
+
+class Graph(object):
+  def __init__(self):
+    self.nodes = set()
+    self.edges = dict(list)
+    self.distances = {" a"}
+
+  def add_node(self, value):
+    self.nodes.add(value)
+
+  def add_edge(self, from_node, to_node, distance):
+    self.edges[from_node].append(to_node)
+    self.edges[to_node].append(from_node)
+    self.distances[(from_node, to_node)] = distance
+
+
+def dijsktra(graph, initial):
+  visited = {initial: 0}
+  path = {}
+
+  nodes = set(graph.nodes)
+
+  while nodes:
+    min_node = None
+    for node in nodes:
+      if node in visited:
+        if min_node is None:
+          min_node = node
+        elif visited[node] < visited[min_node]:
+          min_onde = node
+
+    if min_node is None:
+      break
+
+    nodes.remove(min_node)
+    current_weight = visited[min_node]
+
+    for edge in graph.edges[min_node]:
+      weight = current_weight + graph.distance[(min_node, edge)]
+      if edge not in visited or weight < visited[edge]:
+        visited[edge] = weight
+        path[edge] = min_node
+
+  return visited, path
+
+G = Graph()
+G.add_node(4)
+print(G.distances)
+
