9fe7a96117b856afcb10b8015d68a7ef40d4d219
Louis-Justin TALLOT <lj.tallot@gmail.com>
2020-11-11 20:30:46 +0100

    test de représentation d'une matrice avec matplotplib

--- Dessin_et_fractales/ensemble_de_Mandelbrot.py
+++ Dessin_et_fractales/ensemble_de_Mandelbrot.py
@@ -3,3 +3,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+N = 20  # le nombre de points de tracé
+
+a = np.random.randn(N,N)
+#print(a)
+
+fig, ax =  plt.subplots()
+
+#plt.close()
+ax.matshow(a, cmap = plt.cm.Blues)
+plt.show()
