84bfe1b1f59695b424c4af153adcb3b8f616686d
Louis-Justin TALLOT <lj.tallot@gmail.com>
2020-11-11 23:53:03 +0100

    Une première version qui marche pas mal du tout

--- Dessin_et_fractales/ensemble_de_Mandelbrot.py
+++ Dessin_et_fractales/ensemble_de_Mandelbrot.py
@@ -1,15 +1,46 @@
 """ Ce programme va tracer l'ensemble de Mandelbrot """
 
+"""
+Nous allons faire un np.meshgrid et créer une fonction qui
+renvoie 1 si le point est dans l'ensemble de Mandelbrot 
+et 0 sinon, puis nous la vectoriserons
+
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 
-N = 20  # le nombre de points de tracé
+N = 2000  # le nombre de points de tracé
 
-a = np.random.randn(N,N)
+axe_x = np.linspace(-1.5, 0.5, N)
+axe_y = np.linspace(-1, 1, N)
+
+### tests de représentation d'une matrice sous matplotlib
+#a = np.random.randn(N,N)
+#a = np.zeros((N,N))
 #print(a)
+#fig, ax =  plt.subplots()
+#ax.matshow(a, cmap = plt.cm.Blues)
+#plt.show()
 
-fig, ax =  plt.subplots()
 
-#plt.close()
-ax.matshow(a, cmap = plt.cm.Blues)
+def est_dedans(x,y):
+    c = complex(x,y)
+    z = 0
+    for k in range(100):
+        z = np.power(z,2) + c
+        
+        if abs(z) > 2:
+            return 0
+    return 1
+
+test = np.vectorize(est_dedans)
+
+X, Y = np.meshgrid(axe_x, axe_y)
+
+Z = test(X,Y)
+
+plt.pcolor(X,Y,Z)
+
 plt.show()
+
