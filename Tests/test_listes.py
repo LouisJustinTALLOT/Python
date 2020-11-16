cd41f55d2e35327d57022a797145c9776686d47c
Louis-Justin TALLOT <lj.tallot@gmail.com>
2020-11-16 19:30:49 +0100

    Un test avec les listes pour Musique_classique

---
+++ Tests/test_listes.py
@@ -0,0 +1,11 @@
+# test pour Musique_classique/src/structure_de_document
+# 16.11.2020
+
+ma_liste = [i for i in range(10)]
+
+longueur = len(ma_liste)
+
+print(ma_liste[3:longueur])
+
+# ainsi on a bien que pour avoir une liste jusqu'au bout
+# il faut lui fournir la longueur totale à droite
