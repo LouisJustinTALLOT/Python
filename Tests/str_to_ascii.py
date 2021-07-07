3f195991abbc7514e32e3d9ae459398e9e3c117f
Louis-Justin TALLOT <lj.tallot@gmail.com>
2021-07-07 16:59:16 +0200

    test pour transformer une châine de caractères en ASCII -> échec

---
+++ Tests/str_to_ascii.py
@@ -0,0 +1,10 @@
+my_string = "abcdefghijklmnopqrstuvwxyzéàèùç"
+
+my_string = my_string.replace("à", "a")
+my_string = my_string.replace("é", "e")
+my_string = my_string.replace("è", "e")
+my_string = my_string.replace("ç", "c")
+my_string = my_string.replace("ù", "u")
+# my_string = my_string.replace("", "")
+# my_string = my_string.replace("", "")
+# my_string = my_string.replace("", "")
