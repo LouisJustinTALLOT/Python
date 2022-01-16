422e668ba747587623707f5aa4c5860da7dc0c41
Louis-Justin TALLOT <lj.tallot@gmail.com>
2022-01-16 19:16:13 +0100

    Read directly from a file given as input

--- Utilities/download_pytube_playlists.py
+++ Utilities/download_pytube_playlists.py
@@ -1,15 +1,18 @@
 """Small script to download all videos from a YouTube playlist
+By @LouisJustinTALLOT
 
-    Usage:
-        Simply run the script with the playlist URLs as arguments, separated by spaces.
-        ```python download_pytube_playlists.py <playlist_1_url> <playlist_2_url> ...```
+Usage:
+    If you have only a few playlists to download, you can simply
+    run the script with the playlist URLs as arguments, separated by spaces.
+    ```python download_pytube_playlists.py <playlist_1_url> <playlist_2_url> ...```
 
-        Alternatively, you can write all the playlist URLs to a file, and then pipe
-        the contents of a file to the script:
-        ```cat playlist_urls.txt | python download_pytube_playlists.py```
-        
-    Example:
-        python download_pytube_playlists.py https://www.youtube.com/playlist?list=PLzPiaCaGenuqyZEkfAgUF_CNnNroKKKV7 https://www.youtube.com/playlist?list=PL18D36145BE009EDC
+    In order to download more playlists, you can write all the playlist URLs
+    to a (some) file(s), and then call the script with the name of the file(s)
+    as arguments (you can even mix in youtube urls):
+    ```python download_pytube_playlists.py playlist_urls.txt [playlist_urls_2.txt ...] [youtube playlist urls ...]```
+
+Example:
+    python download_pytube_playlists.py https://www.youtube.com/playlist?list=PLzPiaCaGenuqyZEkfAgUF_CNnNroKKKV7 https://www.youtube.com/playlist?list=PL18D36145BE009EDC
 """
 
 __author__ = "Louis-Justin TALLOT"
@@ -40,12 +43,24 @@ def download_playlist(playlist):
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
-        # pipe from a list of playlist urls
-        playlist_urls_list = sys.stdin.read().split()
+        # print the help
+        print(__doc__)
+        sys.exit()
     else:
         # passed to argv
-        playlist_urls_list = sys.argv[1:]
+        input_args = sys.argv[1:]
 
+    playlist_urls_list = []
+    # we can pass directly urls or a file containing urls
+    for string in input_args:
+        if ".txt" in string or ".md" in string or "youtube" not in string:
+            try:
+                with open(string, "r") as file:
+                    playlist_urls_list.extend(file.read().split())
+            except Exception: # it was not a file
+                pass
+        else:
+            playlist_urls_list.append(string)
 
     playlist_list = [Playlist(url) for url in playlist_urls_list]
 
